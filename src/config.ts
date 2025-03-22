9cb80f2306dc3a463d0eb93f5110a35021de14ac
ryokdy <ryokdy@gmail.com>
2025-03-22 12:27:14 +0900

    Add unit test

--- src/config.ts
+++ src/config.ts
@@ -1,31 +1,110 @@
-import fs from 'fs';
-import path from 'path';
+import { parseArgs } from 'util';
+import * as fs from 'fs';
+import * as path from 'path';
 
 interface Config {
   baseUrl: string;
   apiToken: string;
 }
 
-const configFilePath = path.join(__dirname, 'env', 'config.json');
-
-let config: Config;
-
-if (fs.existsSync(configFilePath)) {
-  try {
-    const fileContent = fs.readFileSync(configFilePath, 'utf-8');
-    config = JSON.parse(fileContent);
-  } catch {
-    throw new Error('Failed to parse the config file.');
-  }
-} else {
-  config = {
-    baseUrl: process.env.BASE_URL!,
-    apiToken: process.env.API_TOKEN!
-  };
-}
+let config: Config = {
+  baseUrl: 'https://sample.runbook.jp',
+  apiToken: ''
+};
 
 export function saveConfig(newConfig: Config) {
+  // create directory if it doesn't exist
+  if (!fs.existsSync(path.dirname(configFilePath))) {
+    fs.mkdirSync(path.dirname(configFilePath), { recursive: true });
+  }
   fs.writeFileSync(configFilePath, JSON.stringify(newConfig, null, 2));
 }
+
+function upperSnakeToCamel(upperSnake: string) {
+  const words = upperSnake.toLowerCase().split('_');
+  return (
+    words[0] +
+    words
+      .slice(1)
+      .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join('')
+  );
+}
+
+function kebabToCamel(kebab: string) {
+  const words = kebab.toLowerCase().split('-');
+  return (
+    words[0] +
+    words
+      .slice(1)
+      .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join('')
+  );
+}
+
+function setConfigFile() {
+  if (fs.existsSync(configFilePath)) {
+    try {
+      const fileContent = fs.readFileSync(configFilePath, 'utf-8');
+      const fileConfig = JSON.parse(fileContent);
+      config = { ...config, ...fileConfig };
+    } catch {
+      throw new Error('Failed to parse the config file.');
+    }
+  }
+}
+
+function setEnvironmentVariables() {
+  const envVars = ['BASE_URL', 'API_TOKEN'];
+  for (const envVar of envVars) {
+    if (process.env[envVar]) {
+      config = { ...config, [upperSnakeToCamel(envVar)]: process.env[envVar]! };
+    }
+  }
+}
+
+function setCommandlineArguments() {
+  const { values } = parseArgs({
+    args: process.argv.slice(2),
+    allowPositionals: true,
+    options: {
+      'api-token': {
+        type: 'string',
+        short: 'n',
+        multiple: false
+      },
+      'base-url': {
+        type: 'string',
+        short: 'h',
+        multiple: false
+      }
+    }
+  });
+
+  for (const key of Object.keys(values)) {
+    const k = key as keyof typeof values;
+    const value = values[k];
+    if (key && value) {
+      config = { ...config, [kebabToCamel(key)]: value };
+    }
+  }
+}
+
+const configFilePath = path.join(__dirname, 'env', 'config.json');
+
+export function loadConfig() {
+  setConfigFile();
+
+  setEnvironmentVariables();
+
+  try {
+    setCommandlineArguments();
+  } catch {
+    // Ignore errors
+  }
+  return config;
+}
+
+loadConfig();
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,8 @@
 #!/usr/bin/env node
 
-import readline from 'readline';
-import { saveConfig } from './config';
 import { runServer } from './server';
 
-const args = process.argv.slice(2);
-
-const ask = (query: string): Promise<string> => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: true
-  });
-
-  return new Promise((resolve) => {
-    rl.question(query, (answer) => {
-      rl.close();
-      resolve(answer);
-    });
-  });
-};
-
-const setConfig = async () => {
-  const baseUrl = (await ask('Enter base URL: ')) || '';
-  const apiToken = (await ask('Enter API token: ')) || '';
-
-  saveConfig({ baseUrl, apiToken });
-};
-
-if (args.includes('--init')) {
-  setConfig().then(() => {
-    console.log('Configuration saved.');
-    process.exit(0);
-  });
-} else {
-  runServer().catch((error) => {
-    console.error('Fatal error running server:', error);
-    process.exit(1);
-  });
-}
+runServer().catch((error) => {
+  console.error('Fatal error running server:', error);
+  process.exit(1);
+});
