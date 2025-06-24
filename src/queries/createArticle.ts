eed99e1708479eb51cd87b34c5aaaf3a5dcd0280
ryokdy <ryokdy@gmail.com>
2025-06-24 14:05:58 +0900

    Add create-article and update-article tools

--- src/queries/createArticle.ts
+++ src/queries/createArticle.ts
@@ -2,18 +2,21 @@ export default `
   mutation createArticle(
     $bookUid: ID!
     $name: String!
+    $bodyMarkdown: String
     $categoryUids: [ID!]
   ) {
     createArticle(
       input: {
         bookUid: $bookUid
         name: $name
+        bodyMarkDown: $bodyMarkdown
         isPublished: true
         categoryUids: $categoryUids
       }
     ) {
       article {
         uid
+        id
       }
       success
       errors {
